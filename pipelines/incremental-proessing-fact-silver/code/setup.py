1a2e54aa1a53cbbbb44c8421c4867496919fdc32
raghav.sreeram@gmail.com <raghav.sreeram@gmail.com>
2025-01-15 14:58:51 +0000

    Renames pipeline from incremental-proessing-fact-silver to incremental-proessing-fact-bronze

--- pipelines/incremental-proessing-fact-silver/code/setup.py
+++ pipelines/incremental-proessing-fact-silver/code/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'incremental-proessing-fact-silver',
+    name = 'incremental-proessing-fact-bronze',
     version = '1.0',
     packages = find_packages(include = ('incrementalproessingfactsilver*', )) + ['prophecy_config_instances'],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
